88175260afa411bc32304e8f4cee97405d39effa
TakehitoNishida <takehito.nishida@gmail.com>
2019-10-22 23:24:21 +0900

    update

--- convert_pdf2.bat
+++
@@ -1,6 +0,0 @@
-set fname=AsciiDoc_Sample_007
-set fname_adc="%fname%.adoc"
-set fname_pdf="%fname%.pdf"
-
-if exist %fname_pdf% (del %fname_pdf%)
-asciidoctor-pdf -r ./add.rb -r asciidoctor-pdf-cjk %fname_adc%
