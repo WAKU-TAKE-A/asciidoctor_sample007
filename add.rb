ca60ffb7fa6963e6fd4ae6bc8872a9a438b820a4
TakehitoNishida <takehito.nishida@gmail.com>
2019-10-23 10:26:38 +0900

    update

--- add.rb
+++
@@ -1,6 +0,0 @@
-Prawn::SVG::FontRegistry.font_path << "c:/Fonts"
-
-Prawn::Svg::Font::GENERIC_CSS_FONT_MAPPING.merge!(
-  'asana-math' => 'c:/Fonts/asanamath.ttf',
-  'sans-serif' => 'c:/Fonts/sansserif.ttf'
-)
