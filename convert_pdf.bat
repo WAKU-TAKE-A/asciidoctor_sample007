3d6a50aec50c008504f1b18c53a3380200cf5e48
TakehitoNishida <takehito.nishida@gmail.com>
2019-11-03 14:47:41 +0900

    update

--- convert_pdf.bat
+++ convert_pdf.bat
@@ -1,8 +1,6 @@
-set fname=AsciiDoc_Sample_007
+set fname=AsciiDoc_Sample_006
 set fname_adc="%fname%.adoc"
 set fname_pdf="%fname%.pdf"
 
 if exist %fname_pdf% (del %fname_pdf%)
-asciidoctor-pdf -r asciidoctor-pdf-cjk-kai_gen_gothic %fname_adc%
-rem When not using kai_gen_gothic
-rem asciidoctor-pdf -r asciidoctor-pdf-cjk %fname_adc%
+asciidoctor-pdf -r asciidoctor-pdf-cjk %fname_adc%
